fe8c5d1c300b13c0563f0b4df53730f7ff508b90
Ria Mehta <rmehta4407@gmail.com>
2026-03-03 01:21:20 +0530

    Create Multiply_Strings.py

---
+++ Multiply_Strings.py
@@ -0,0 +1,9 @@
+def multiply(num1, num2):
+        int_1 = int(num1)
+        int_2 = int(num2)
+        return str(int_1 * int_2) 
+
+num1 = input("enter string 1:")
+num2 = input("enter string 2:")
+output = multiply(num1, num2)
+print(output)
